dea5b454d189df3b7c1a80df1c2e27bf62d7e534
Mobb autofixer <git@mobb.ai>
2024-04-08 15:28:58 +0000

    Log Forging fix by mobb-8ae83d9d-c7a2-4727-8354-5d65da652f55

--- logForginSample.java
+++ logForginSample.java
@@ -1,5 +1,5 @@
 public class logForginSample {
   public void doNothing(HttpServletRequest request) {
-      log.trace("Incoming request for: {}", request.getRequestURL());
+      log.trace("Incoming request for: {}", String.valueOf(request.getRequestURL()).replace("\n", "").replace("\r", ""));
   }
 }
